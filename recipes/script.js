fbad1b1546bc754015b68b1f6537557b5c909c34
Justin Kelley <143836549+JKelley86@users.noreply.github.com>
2025-03-09 00:58:13 -0600

    Update script.js

--- recipes/script.js
+++ recipes/script.js
@@ -14,16 +14,4 @@
                 alert("Sharing is not supported on this device.");
             }
         }
-window.onload = function() {
-    const modal = document.getElementById('nutrition-modal');
-    modal.style.display = 'none'; // Ensure modal is hidden on page load
-}
 
-function toggleInfo() {
-    const modal = document.getElementById('nutrition-modal');
-    if (modal.style.display === 'none' || modal.style.display === '') {
-        modal.style.display = 'flex';
-    } else {
-        modal.style.display = 'none';
-    }
-}
