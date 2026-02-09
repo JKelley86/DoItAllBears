776842745cd9a8177d728298ad5b960f99fac7f0
Justin Kelley <143836549+JKelley86@users.noreply.github.com>
2026-02-09 07:36:19 -0600

    Delete games/testgame/data.js

--- games/testgame/data.js
+++
@@ -1,8 +0,0 @@
-const STATE_DATA = {
-  California:{money:300,oil:100,steel:200},
-  Texas:{money:350,oil:300,steel:150},
-  Minnesota:{money:200,oil:80,steel:220},
-  Florida:{money:250,oil:60,steel:120},
-  NewYork:{money:320,oil:50,steel:140},
-  Washington:{money:220,oil:70,steel:180}
-};
