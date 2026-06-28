941d2b24bbcc19e286dbb070c7ffe73d3eb35506
Justin Kelley <143836549+JKelley86@users.noreply.github.com>
2026-06-27 22:36:42 -0500

    Add configuration file for Kelley Corner app

---
+++ kelley-corner/js/config.js
@@ -0,0 +1,12 @@
+export const config = {
+
+    appName: "Kelley Corner",
+
+    pocketbaseURL:
+        "https://tumid-kiltlike-maia.ngrok-free.dev",
+
+    postExpireDays: 30,
+
+    maxImages: 10
+
+}
